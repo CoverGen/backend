f5a19f1fa57d98585475eb4e104125d3ac0c59ee
Erick Velasco <fervel1704@gmail.com>
2023-04-12 13:04:02 -0400

    Fixes on Readme.md, settings files and ticket_app's models.py

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "CoverGen",
+        "NAME": "covergen",
         "USER": "covgen_admin",
         "PASSWORD": "covgen_admin",
         "HOST": "localhost",
