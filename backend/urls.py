48cc2529ad223064c1c623d0f1fb1dd5c61c70e0
giterick221 <3rick221@gmail.com>
2023-04-19 01:32:15 -0400

    Added basic endpoint logic for register, login POST requests.

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ticket_app.use_cases.accounts import views as accounts
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # accounts paths
+    path("register/", accounts.register),
+    path("login/", accounts.login),
 ]
